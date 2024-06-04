1e5560966baac14d0fb76952a71098b720b5cbd8
jevillada <juanesvillada.o@gmail.com>
2024-06-04 14:59:39 -0500

    se agrega  el proyecto

---
+++ script.js
@@ -0,0 +1,37 @@
+let menuVisible = false;
+//oculta muestra menu 
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar(){
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("htmlcss");
+        habilidades[2].classList.add("java");
+        habilidades[3].classList.add("comunicacion");
+        habilidades[4].classList.add("trabajo");
+        habilidades[5].classList.add("creatividad");
+        habilidades[6].classList.add("liderazgo");
+        habilidades[7].classList.add("dedicacion");
+
+    }
+}
+
+window.onscroll = function(){
+    efectoHabilidades();
+}
